a7b4a1254844db62ac7d89c9635b094b97aaf87b
Jenny Shamoo <jenny.shamoo@gmail.com>
2020-03-28 17:24:51 -0700

    updated app.ts

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,51 +1,47 @@
 import express from 'express';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
-import multer from 'multer';
-import fs from 'fs';
+import handleUpload from './router';
 import { Transactions, Categories } from './db/index';
 
 const app = express();
 app.use(morgan('tiny'));
 app.use(express.static('dist/client'));
 
-const upload = multer({ dest: 'uploads/' });
-app.post('/api/upload', upload.array('fin'), (req, res) => {
-  console.log(req.files)
-  res.sendStatus(201);
-})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/api', (req, res) => {
   Transactions.findAll({include: [Categories]})
-    .then((docs) => res.send(docs))
-    .catch(err => console.error(err))
+  .then((docs) => res.send(docs))
+  .catch(err => console.error(err))
 });
 
 app.get('/api/trans', (req, res) => {
   Transactions.findAll()
-    .then((docs) => {
-      res.send(docs);
-    })
-    .catch(err => console.error(err))
+  .then((docs) => {
+    res.send(docs);
+  })
+  .catch(err => console.error(err))
 })
 
 app.get('/api/cate', (req, res) => {
   Categories.findAll()
-    .then((docs) => {
-      res.send(docs);
-    })
-    .catch(err => console.error(err))
+  .then((docs) => {
+    res.send(docs);
+  })
+  .catch(err => console.error(err))
 })
 
 app.patch('/api/trans/:id', (req, res) => {
   Transactions.update(req.body, {
     where: req.params
   })
-    .then(() => res.sendStatus(201))
-    .catch((err) => console.error(err));
+  .then(() => res.sendStatus(201))
+  .catch((err) => console.error(err));
 })
+
+app.post('/api/upload', handleUpload);
 
 app.listen('5000', () => {
   console.log('Express Server is listening on 5000');
