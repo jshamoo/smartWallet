377341cf49de051c05803dbfd098f68b9a843cfc
Jenny Shamoo <jenny.shamoo@gmail.com>
2020-03-29 23:54:21 -0700

    added interface

--- src/client/demoData.ts
+++ src/client/demoData.ts
@@ -1,14 +1,21 @@
-export const trans: Array<Object> = [
-  { "id": 1, "date": "2019-08-20", "amount": 100.82, "description": "Water Bill" },
-  { "id": 2, "date": "2019-08-28", "amount": 137.08, "description": "Raphl" },
-  { "id": 3, "date": "2020-01-10", "amount": 209.97, "description": "Car Insurance" },
-  { "id": 4, "date": "2019-06-18", "amount": 58.91, "description": "Garden" },
-  { "id": 5, "date": "2020-03-03", "amount": 53.48, "description": "Books" },
-  { "id": 6, "date": "2019-10-29", "amount": 7.34, "description": "Clothing" },
-  { "id": 7, "date": "2019-06-03", "amount": 288.59, "description": "Clothing" },
-  { "id": 8, "date": "2019-04-05", "amount": 19.85, "description": "Pizza D'oro" },
-  { "id": 9, "date": "2019-05-30", "amount": 97.45, "description": "Toaster Oven" },
-  { "id": 10, "date": "2020-01-14", "amount": 64.44, "description": "Shoes" },
+interface Record {
+  Date: string,
+  Amount: number,
+  Description: string,
+  Category: string
+}
+
+export const trans: Array<Record> = [
+  { "Date": "2019-08-20", "Amount": 100.82, "Description": "Water Bill", "Category": "Bill"},
+  { "Date": "2019-08-28", "Amount": 137.08, "Description": "Raphl", "Category": "Grocery" },
+  { "Date": "2020-01-10", "Amount": 209.97, "Description": "Car Insurance", "Category": "Bill" },
+  { "Date": "2019-06-18", "Amount": 58.91, "Description": "Garden", "Category": "Hobby" },
+  { "Date": "2020-03-03", "Amount": 53.48, "Description": "Books", "Category": "Hobby"},
+  { "Date": "2019-10-29", "Amount": 7.34, "Description": "Clothing", "Category": "Clothing" },
+  { "Date": "2019-06-03", "Amount": 288.59, "Description": "Clothing", "Category": "Clothing"},
+  { "Date": "2019-04-05", "Amount": 19.85, "Description": "Pizza D'oro", "Category": "Food"},
+  { "Date": "2019-05-30", "Amount": 97.45, "Description": "Toaster Oven", "Category": "House" },
+  { "Date": "2020-01-14", "Amount": 64.44, "Description": "Shoes", "Category": "Clothing" },
 ];
 
 export const cate: Array<Object> = [
